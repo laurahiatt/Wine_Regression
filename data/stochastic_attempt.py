0e1585b92426dfb71a64467da94d9dfe6e1a91d4
laurahiatt <lahiatt@davidson.edu>
2016-01-23 01:41:13 -0500

    updates

--- data/stochastic_attempt.py
+++ data/stochastic_attempt.py
@@ -1,4 +1,5 @@
-import numpy
+import numpy as np
+import random
 import csv
 from numpy import matrix
 from numpy import linalg
@@ -8,13 +9,14 @@ FIELDS = ["fixed acidity", "volatile acidity","citric acid","residual sugar",
           "chlorides","free sulfur dioxide","total sulfur dioxide","density",
           "pH","sulphates","alcohol","quality"]
 
-with open('winequality-red.csv', 'r') as csvfile:
+with open('winequality-white.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=';', fieldnames = FIELDS)
     reader.next()
     count = 0
+    index = 0
     train = [None] * 2938
-    validation = [None] * 20
-    test = [None] * 20
+    validation = [None] * 980
+    test = [None] * 980
     train_answer = [0.0] * 2938
     validation_answer = [0.0] * 980
     test_answer = [0.0] * 980
@@ -26,18 +28,62 @@ with open('winequality-red.csv', 'r') as csvfile:
                 float(row["pH"]),float(row["sulphates"]),float(row["alcohol"])]        
         
         if count < 2938:
-            train[count] = entry
-            train_answer[count] = float(row["quality"])
-        elif count < 3917:
-            validation[count] = entry
-            validation_answer[count] = float(row["quality"])
+            train[index] = entry          
+            train_answer[index] = float(row["quality"])
+            
+        elif count >= 2938 and count < 3917:
+            validation[index] = entry
+            validation_answer[index] = float(row["quality"])
         else:
-            test[count] = entry
-            test_answer[count] = float(row["quality"])
+            test[index] = entry
+            test_answer[index] = float(row["quality"])
+        if (count == 2937) or (count == 3917):
+            index = 0
+        else:
+            index += 1
         count += 1
+    
     train_set = np.matrix(train)
     validation_set = np.matrix(validation)
     test_set = np.matrix(test)
+    
     train_answer_set = np.matrix(train_answer)
     validation_answer_set = np.matrix(validation_answer)
     test_answer_set = np.matrix(test_answer)
+    print train_set
+    print train_answer_set
+    t=  test_answer_set.transpose()
+    print t.item(979)
+    
+    #attempt to put all in range
+    train_min = train_set.min(axis = 0)
+    train_max = train_set.max(axis = 0)
+    train_set = (train_set - train_min)/(train_max - train_min)
+
+    
+   # validation_min = validation_set.min(axis = 0)
+   # validation_max = validation_set.max(axis = 0)
+   # validation_set = (validation_set - validation_min)/(validation_max - validation_min)    
+    
+    #validation_min = validation_set.min(axis = 0)
+    #validation_max = validation_set.max(axis = 0)
+    #validation_set = (validation_set - validation_min)/(validation_max - validation_min)    
+
+    test_min = test_set.min(axis = 0)
+    test_max = test_set.max(axis = 0)
+    test_set = (test_set - test_min)/(test_max - test_min)  
+    
+    
+    #Building the tests
+    for i in range(1, 7): #degree of polynomial
+            for j in range(60): #60 random initializations of theta
+                thetas = [0.0] * (i + 1) #add one for intercept term
+                for k in range(i + 1): #set values of theta
+                    thetas[k] = random.random() * random.randrange(50)
+                epsilon = 0.1
+                alpha = 2
+                for l in range(2938):
+                    for m in range(i + 1):
+                        temp = thetas[m] + 
+                    
+    
